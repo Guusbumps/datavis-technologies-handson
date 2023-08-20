0137acf9668dc467f725778a2f76b4710939d3ab
Guusbumps <guus.spenkelink@student.uhasselt.be>
2023-08-20 22:45:27 +0200

    ex1

--- src/routes/02_exercises/01_web_technologies/exercise_01/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_01/Exercise.svelte
@@ -1,7 +1,3 @@
-<div class="answer">My name is <b>Guus</b></div>
-
-<style>
-  div.answer {
-    color: rgb(2, 96, 45);
-  }
-</style>
+<div class="answer" style="color: rgb(2, 96, 45)">
+  My name is <b style="color: rgb(255, 0, 0)">Guus</b>
+</div>
