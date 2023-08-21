e9d7639e54362d9105af7fdceec5b332783aaecc
Guusbumps <guus.spenkelink@student.uhasselt.be>
2023-08-21 18:42:27 +0200

    ex14

--- src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
+++ src/routes/02_exercises/01_web_technologies/exercise_14/Exercise.svelte
@@ -3,14 +3,29 @@
 </script>
 
 <!-- We will cover buttons later on! -->
-<button class="btn btn-secondary" on:click={() => (current = "messi")}>
+<button 
+  class="btn btn-secondary" 
+  class:lastClicked={current === "messi"}
+  on:click={() => (current = "messi")}>
   Messi
 </button>
 
-<button class="btn btn-secondary" on:click={() => (current = "ronaldo")}>
+<button 
+  class="btn btn-secondary" 
+  class:lastClicked={current === "ronaldo"}
+  on:click={() => (current = "ronaldo")}>
   Ronaldo
 </button>
 
-<button class="btn btn-secondary" on:click={() => (current = "neymar")}>
+<button 
+  class="btn btn-secondary" 
+  class:lastClicked={current === "neymar"}
+  on:click={() => (current = "neymar")}>
   Neymar
 </button>
+
+<style>
+  .lastClicked {
+    background-color: red;
+  }
+</style>
