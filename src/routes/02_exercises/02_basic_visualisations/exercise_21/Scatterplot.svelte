d1f2141e7007301eab692fa9b633acd4edbf4e77
Guusbumps <guus.spenkelink@student.uhasselt.be>
2023-08-22 23:05:48 +0200

    ex21

--- src/routes/02_exercises/02_basic_visualisations/exercise_21/Scatterplot.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_21/Scatterplot.svelte
@@ -4,11 +4,19 @@
     const margin = { top: 50, right: 5, bottom: 55, left: 50 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
+
+    export let data = []
+
+    import { scaleLinear } from 'd3-scale'
+    const xscale = scaleLinear().domain(0,100000).range(0, innerWidth)
+    const yscale = scaleLinear().domain(0,100).range(0, innerHeight)
   </script>
   
   <svg viewBox="0 0 {width} {height}" style="max-width: {width}px">
     <g transform="translate({margin.left}, {margin.top})">
-      <!--  -->
+      {#each data as d}
+        <circle cx={xscale(d.income)} cy={yscale(d.life_exp)} r=10/>
+      {/each}
     </g>
   </svg>
   
