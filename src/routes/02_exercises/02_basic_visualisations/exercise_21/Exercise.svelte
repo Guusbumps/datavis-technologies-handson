10256eb09dbedac0239393d9c78dfb4cc3a3907c
Guusbumps <guus.spenkelink@student.uhasselt.be>
2023-08-22 22:32:27 +0200

    ex21

--- src/routes/02_exercises/02_basic_visualisations/exercise_21/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_21/Exercise.svelte
@@ -1,3 +1,14 @@
 <script>
+    import {csv} from 'd3-fetch'
+    import ScatterPlot from './Scatterplot.svelte'
+    
+    let data = null;
+    onMount(async () => {
+        console.log("onMount started!");
+        data = await csv('/static/data/gapminder.json');
+        console.log("Data loaded!");
+    });
+    console.log("Script finished!")
+
 
 </script>
