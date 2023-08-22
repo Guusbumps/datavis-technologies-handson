2d189a4e5893835d3a363fb3754f71fb80bfbaf7
Guusbumps <guus.spenkelink@student.uhasselt.be>
2023-08-22 22:11:34 +0200

    ex20

--- src/routes/02_exercises/02_basic_visualisations/exercise_20/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_20/Exercise.svelte
@@ -5,7 +5,10 @@
     const margin = { top: 10, right: 10, bottom: 30, left: 60 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
-  
+
+    import { scaleLinear } from 'd3-scale'
+    import { scaleOrdinal } from 'd3-scale'
+
     // Array
     const data = [
       { service: "Netflix", viewers: 2.9 },
@@ -15,12 +18,23 @@
       { service: "Apple TV", viewers: 1.1 },
       { service: "Rakuten", viewers: 0.4 }
     ];
+
+    const xscale = scaleBand().domain(data.map((d) => d.service)).range([0, innerWidth]).padding(0.2);
+    const yscale = scaleLinear().domain(0, 4).range(0, innerHeight)
+
   </script>
   
   <!-- setting a viewBox and max-width allows the SVG to shrink but not grow! -->
   <svg viewbox="0 0 {width} {height}" style="max-width: {width}px">
     <g transform={`translate(${margin.left},${margin.top})`}>
-      <!--  -->
+      {#each data as d}
+        <rect
+          x = {xscale(d.service)}
+          y = {yscale(d.viewers)}
+          width = 1
+          height = {yscale(d.viewers)}
+        />
+      {/each}
     </g>
   </svg>
   
