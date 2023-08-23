e6edc0d1a45b62bab3b1edbde0a143fe767c50fb
Guusbumps <guus.spenkelink@student.uhasselt.be>
2023-08-23 11:56:57 +0200

    ex22

--- src/routes/02_exercises/03_interactivity/exercise_22/Exercise.svelte
+++ src/routes/02_exercises/03_interactivity/exercise_22/Exercise.svelte
@@ -1,3 +1,18 @@
 <script>
-    
+    let count = 0
+    function add(event) {
+        count += 1
+        console.log("Clicked!");
+        }
+    function reset(event) {
+        count = 0;
+        console.log("Clicked!");
+    }
 </script>
+
+<button class="btn btn-primary" on:click={add}>add</button>
+<button class="btn btn-primary" on:click={reset}>reset</button>
+
+<span>
+Number of times clicked = {count}
+</span>
